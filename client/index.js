a6584736420f62a54ae9614d46ba1bb10952f28b
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2022-03-11 21:23:14 -0800

    deployment test

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
-fetch("")
+fetch("https://northeastern-university-343405.uw.r.appspot.com/users")
   .then((response) => {
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
